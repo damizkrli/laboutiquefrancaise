5352b7582c26eb8907e68dc1910ac76fd603c6b4
Damien Carli <karli.damien@gmail.com>
2022-06-10 09:53:34 +0200

    Création de la vue par produit, mise en place du slug, FRONT

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/nos-produits', name: 'products')]
+    public function index(): Response
+    {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'product')]
+    public function show($slug)
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBy(['slug' => $slug]);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+}
