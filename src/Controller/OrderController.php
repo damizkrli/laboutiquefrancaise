9ef3d80e2b386e9f189a7c15988c812a44e5f345
Damien Carli <karli.damien@gmail.com>
2022-06-12 16:22:26 +0200

    Tunnel d'achat : Sommaire de la commande et ajout de style

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     #[Route('/commande', name: 'order')]
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('account_adress_add');
@@ -20,8 +22,15 @@ class OrderController extends AbstractController
             'user' => $this->getUser(),
         ]);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
+            'cart' => $cart->getFull(),
         ]);
     }
 }
